0f98a75f989b0de2d8a6c73e8bcd3f6243678eff
c0d3f0rc3 <r09922106@ntu.edu.tw>
2022-05-18 09:21:00 +0800

    key_database with prefix completed

--- src/key_database.py
+++ src/key_database.py
@@ -2,10 +2,9 @@ from Crypto.PublicKey import RSA
 import json
 
 # database should record userid, gid, pubKey
-# group should be disjoint
-# send from authenticator: 
-# {'role': 'authenticator', 'action': 'get_pubkeys', 'group': gid}
-# group id: gid
+# groups should be disjoint
+# json received from authenticator: 
+# {'role': 'authenticator', 'action': 'get_pubkeys', 'group': gid, 'prefix': prefix}
 
 groups = [
          [10, 27, 43, 44, 51, 66, 67, 69, 74, 88], 
@@ -20,14 +19,27 @@ groups = [
          [4, 5, 16, 21, 22, 60, 64, 73, 89, 98]
         ]
 
-def get_pubkeys(gid):
+def get_pubkeys(gid, prefix = 32):
     """
-    Given a group id, return public keys in this group in the form:
+    Given a gid and a prefix, return public keys of these groups in the form:
     [{'userid': uid, 'pubkey': pubKey_uid}, {'userid': uid, 'pubkey': pubKey_uid},...]
     where pubKey_uid is a utf-8 string decoded from bytes
     """
+    uid_list = []
+    # Suppose prefix only within 0 ~ 32
+    if 0 <= prefix and prefix <= 31:
+        mask = 2 ** (32 - prefix)
+        mod = gid % mask
+        start = gid - mod # Suppose gid <= 9
+        stop = min(start+mask, len(groups))
+        for i in range(start, stop):
+            uid_list += groups[i]
+        uid_list.sort()
+    else:
+        uid_list += groups[gid]
+        
     pub_keys = []
-    for uid in groups[gid]:
+    for uid in uid_list:
         file_pubKey = f"../public/user{uid}.pem"
         with open(file_pubKey, 'r') as f_pub:
             pubKey = RSA.import_key(f_pub.read())
@@ -38,7 +50,7 @@ def get_pubkeys(gid):
 def req_pubkeys(pkt):
     """
     The authenticator request all public keys from database in the form:
-    {'role': 'authenticator', 'action': 'get_pubkeys', 'group': gid}
+    {'role': 'authenticator', 'action': 'get_pubkeys', 'group': gid, 'prefix': prefix}
     return selected public keys with gid in the form:
     {'role': 'database', 'pubkeys': pubkeys}
     """
@@ -46,11 +58,11 @@ def req_pubkeys(pkt):
     assert req['role'] == 'authenticator'
     assert req['action'] == 'get_pubkeys'
     gid = req['group']
-    # prefix = pkt['prefix'] # not yet implemented for now
-    return json.dumps({'role': 'database', 'pubkeys': get_pubkeys(gid)})
+    prefix = req['prefix']
+    return json.dumps({'role': 'database', 'pubkeys': get_pubkeys(gid, prefix)})
 
 # # Testing the API
-# pkt = json.dumps({'role': 'authenticator', 'action': 'get_pubkeys', 'group': 9})
+# pkt = json.dumps({'role': 'authenticator', 'action': 'get_pubkeys', 'group': 8, 'prefix': 29})
 # res = req_pubkeys(pkt)
 # data = json.loads(res)
 # print(data['role'])
@@ -59,3 +71,5 @@ def req_pubkeys(pkt):
 # for pub_key in data['pubkeys']:
 #     print(pub_key['userid'])
 #     print(pub_key['pubkey'].encode()) # get the original bytes encoded pubkey
+# print()
+# print(len(data['pubkeys']))
